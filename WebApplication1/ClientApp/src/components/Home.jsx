10634fe6f5f066a352d8f2b49c214e61f601f68e
Tom Johnson <tom.johnson@softwire.com>
2019-01-04 10:38:45 +0000

    XD

---
+++ WebApplication1/ClientApp/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+
+
+    render() {
+
+        var displayName = Home.name;
+
+        return (
+            <div>
+                <h1>Tube Journey Planner</h1>
+            </div>
+            );
+    }
+}
